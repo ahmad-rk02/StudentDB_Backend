3a2466373792d4cf3e9662c243deb25377c1f30f
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-04-21 00:37:42 +0530

    Initial commit

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
   port: 5432,
   ssl: {
     require: true,
