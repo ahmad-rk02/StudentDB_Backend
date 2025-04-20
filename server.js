273298b3b9693057a39d23e15b01c43a5f4c9ebd
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-04-20 21:51:25 +0530

    added

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use('/api', studentRoutes);
 app.use('/api/auth', authRoutes);
 
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
